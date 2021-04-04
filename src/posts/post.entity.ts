6989e26bbbfe9e5d22f3b9cd50806ac564c055aa
Vladimir Egorov <mekitojizzz@gmail.com>
2021-04-04 22:28:28 +0300

    add index to post author

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -5,6 +5,7 @@ import {
   ManyToOne,
   JoinTable,
   ManyToMany,
+  Index,
 } from 'typeorm';
 import { Category } from '../categories/category.entity';
 import { User } from '../users/user.entity';
@@ -23,6 +24,7 @@ export class Post {
   @Column({ nullable: true })
   public category?: string;
 
+  @Index('post_authorId_index')
   @ManyToOne(() => User, (author: User) => author.posts)
   public author?: User;
 
