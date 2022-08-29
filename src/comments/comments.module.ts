4975784d0fe22d38c9f57ce6a7369104ee157c05
Vladimir Egorov <mekitojizzz@gmail.com>
2022-08-29 23:06:44 +0300

    Add GetCommentHandler to providers

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,10 +3,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateCommentHandler } from './commands/create-comment.handler';
 import { CommentsController } from './comments.controller';
+import { GetCommentHandler } from './queries/get-comment.handler';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment]), CqrsModule],
   controllers: [CommentsController],
-  providers: [CreateCommentHandler],
+  providers: [CreateCommentHandler, GetCommentHandler],
 })
 export class CommentsModule {}
